6b31e9b04e6154ec9b6ee6a69f970461dca39b35
Safia Chettih <31898239+safiamc@users.noreply.github.com>
2023-01-02 14:58:06 -0600

    Create group_byii.sql
    Answer first question of Group By Part II quiz in Aggregations section.

---
+++ Aggregations/group_byii.sql
@@ -0,0 +1,21 @@
+/* For each account, determine the average amount of each type of paper they purchased across their orders.
+Your result should have four columns:
+one for the account name and one for the average quantity purchased for each of the paper types for each account.
+*/
+SELECT a.name, AVG(standard_qty) AS standard_avg,
+AVG(gloss_qty) AS gloss_avg,
+AVG(poster_qty) AS poster_avg
+FROM orders o
+JOIN accounts a ON o.account_id = a.id
+GROUP BY a.name
+/* Determine the number of times a particular channel was used in the web_events table for each sales rep.
+Your final table should have three columns:
+the name of the sales rep, the channel, and the number of occurrences.
+Order your table with the highest number of occurrences first.
+*/
+SELECT s.name AS rep_name, w.channel, COUNT(channel) as occurrences
+FROM web_events w
+JOIN accounts a ON w.account_id = a.id
+JOIN sales_reps s ON a.sales_rep_id = s.id
+GROUP BY s.name, w.channel
+ORDER BY occurrences DESC
