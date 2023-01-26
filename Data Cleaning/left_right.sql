0070d7e677c8a97b2bbe6faf3f10c6b4407dc161
Safia Chettih <31898239+safiamc@users.noreply.github.com>
2023-01-26 16:40:28 -0600

    Complete quiz questions

--- Data Cleaning/left_right.sql
+++ Data Cleaning/left_right.sql
@@ -13,6 +13,17 @@ GROUP BY 1
 ORDER BY 1
 /* Use the accounts table and a CASE statement to create two groups: one group of company names that start with a number and the second group of those company names that start with a letter.
 What proportion of company names start with a letter? */
-
+SELECT
+CASE WHEN LEFT(name,1) IN ('0','1','2', '3', '4', '5', '6', '7', '8', '9') THEN 0
+ELSE 1 END AS letter, COUNT(*)
+FROM accounts
+GROUP BY 1
+/* 350/351~99.7% of company names start with a letter.*/
 /* Consider vowels as a, e, i, o, and u.
 What proportion of company names start with a vowel, and what percent start with anything else? */
+SELECT
+CASE WHEN LEFT(name,1) IN ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U') THEN 1
+ELSE 0 END AS vowel, COUNT(*)
+FROM accounts
+GROUP BY 1
+/* 80/351~22.8% start with a vowel and  271/351~77.2% start with anything else.*/
